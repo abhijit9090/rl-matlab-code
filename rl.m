15d6fdebb81c0cb9df59f80b85a9e3cd0d10ab6f
abhijit9090 <43819965+abhijit9090@users.noreply.github.com>
2018-11-05 19:23:09 +0530

    Add files via upload

---
+++ rl.m
@@ -0,0 +1,22 @@
+clc
+clear all
+Iin=10
+l=10
+r=1000
+T=l/r
+t=0:0.002:0.05;
+Iout=Iin*exp(-t/T);
+plot(t,Iout)
+xlabel("time")
+ylabel("amplpitue")
+l1=8
+l2=6
+T1=l1/r
+T2=l2/r
+Iout1=Iin*exp(-t/T1);
+Iout2=Iin*exp(-t/T2);
+hold on
+plot(t,Iout1)
+plot(t,Iout2)
+legend('l=10','l1=8','l2=6')
+print('plot','-dpdf')
